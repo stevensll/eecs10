1b4b65e087085a9d41c924f32447415cbe81ded1
steven <deltaorb637@gmail.com>
2024-06-21 15:33:22 -0700

    game working

--- ee10b/hw5 hexer game/src/HexerGame.asm
+++ ee10b/hw5 hexer game/src/HexerGame.asm
@@ -92,9 +92,38 @@
     ;             SwitchPattern = GetSwitches()   ; this is blocking until a switch is pressed
     ;             HandleSwitchPress(SwitchPattern) ; t
     ;             GameOver(MoveCounter, CurrGameLEDsPatt)
+    ;             break;  
     ;         }
     ;     }
 	; }
+
+
+GameOverState:
+
+DisplayGameOverMessage:
+    LDS     R16, GameOverMessage
+    LDS     R17, GameOverMessage+1
+    RCALL   DisplayHex
+
+GameOverStateLoop:
+    RCALL   GetSwitches
+    CPI     R16, START_RANDOM_RESET
+    BREQ    RandomResetGame    
+    CPI     R16, START_MANUAL_RESET0
+    ; handle reset
+    CPI     R16, START_MANUAL_RESET1   
+    RJMP    GameOverStateLoop
+
+RandomResetGame:
+    RCALL   ResetMoveCounter
+    RCALL   ResetGameLEDs
+
+DoneGameOverState:
+    LDI     R16, GAME_PLAY_STATE
+    STS     GameState, R16     
+    RET 
+
+
 
 
 ; HandleSwitchPress:
@@ -149,67 +178,135 @@ RunSwitchHandler:                   ; set arguments for functions to update LEDs
 DoneHandleSwitchPress:
 	RET
 
-
+;;;;;;;;;;;;;;;;;;;;;;;
 HexerPlayGameLoop:
 
+SetupDisplays:
 	RCALL   ClearDisplay            ; clear the display
 	RCALL   Delay16
-	LDS		R16, MoveCounter
+   
+
+RunGameStateLoop:
+    LDS     R16, GameState
+    CPI     R16, GAME_PLAY_STATE
+    BREQ    DoGamePlayState
+    CPI     R16, GAME_OVER_STATE
+    BREQ    DoGameOverState
+    RCALL   GameOverState
+
+DoGamePlayState:
+    RCALL   GamePlayState
+    RJMP    RunGameStateLoop
+
+DoGameOverState:
+    RCALL   GameOverState
+    RJMP    RunGameStateLoop
+
+DoneHexerPlayGameLoop:
+    RET 
+    
+
+
+GamePlayState:
+
+DisplayMoveCounter:                 ; display the number of moves left to play
+    LDS		R16, MoveCounter            ; load the move counter
 	LDS		R17, MoveCounter+1
-	RCALL	DisplayHex				; 
-    RCALL   ResetGameLEDs           
+	RCALL	DisplayHex		            ; and display it
 
-SwitchWaitLoop:                     ; loop until there is a switch press
-    RCALL   GetSwitches             ; wait until a switch is avaiable (blocking)
-    ;RJMP   HandleUserInput         ; switch is pressed, handle the input
+HandleUserInput:                  ; loop until there is a switch press
+    
+	RCALL   GetSwitches             ; wait until a switch is avaiable (blocking)    
+    RCALL   HandleSwitchPress       ; update the game based on the switch press
+    RCALL   GameOver                ; and update the game state if win or loss
 
-HandleUserInput:
-    RCALL   HandleSwitchPress   
-
-UpdateGameState:                    ; check if the player has now win/lost 
-    RCALL   GameOver                    ; and update the game state if so
-
-RestartHexerPlayGameLoop:           ; restart the play loop to loop forever 
-    JMP    SwitchWaitLoop               ; loop forever for user input
-    RET                                 ; should never get here
+DoneGamePlayState:                  ; done with game play state 
+    RET                                 ; and return
+                                        ; NOTE: this procedure should be called
+                                        ; in a loop!
 
 
 
+; ;;;;;;;;;ResetGameLEDs
+ResetGameLEDs:                          
 
-; ResetGameLEDs
-
-ResetGameLEDs:
-
-InitGameLEDs:
-    LDI     R16, LOW(BLUE_LEDS_MASK)
-    STS     CurrGameLEDsPatt, R16
-    LDI     R17, HIGH(BLUE_LEDS_MASK)
+GetDefaultGameLEDsPattern:              ; get the default game LEDs pattern
+    LDI     R16, LOW(DEFAULT_RESET_GAME_LEDS_PATT)  ; load the default pattern
+    LDI     R17, HIGH(DEFAULT_RESET_GAME_LEDS_PATT)      
+    STS     CurrGameLEDsPatt, R16           ; store it as current pattern
     STS     CurrGameLEDsPatt+1, R17
-    RCALL   DisplayGameLEDs
+    
+DisplayDefaultGameLEDsPattern:          ; display the default game LEDs pattern
+    RCALL   DisplayGameLEDs                 
 
-DoneResetGameLeds:
-    RET
+DoneResetGameLEDs:                      ; done resetting game LEDs
+    RET                                     ; and return
 
+; ;;;;;;;;;ResetMoveCounter
+ResetMoveCounter:
+
+GetInitialMoveCount:                    ; get the intial move count
+    LDI     R16, LOW(GAME_INIT_MOVE_COUNT)   ; load the initial move count
+    LDI     R17, HIGH(GAME_INIT_MOVE_COUNT)     
+    STS     MoveCounter, R16                 ; store it to the move count
+    STS     MoveCounter+1, R17
+
+DisplayInitialMoveCount:                ; display the inital move counter
+    RCALL   DisplayHex                         
+
+DoneResetMoveCounter:                   ; done resetting the move counter
+    RET                                     ; so return
  
 
-;;
+; GameOver(CurrGameLEDsPatt, MoveCounter){
+    
+;     IF (CurrGameLEDsPatt == GAME_OVER_LED_PATT){
+;         GameOverMessage = "WIN"
+;         GameState = GameOver
+;         RETURN
+;     } ELSE IF (MoveCounter == 0) {
+;         GameOverMessage = "LOSE"
+;         GameState = GameOver
+;         RETURN
+;     } 
+;     RETURN
+; }
 
 GameOver:
 
-CheckGameLEDsPatt:                      ; check if there are no more LEDs on     
+CheckGameLEDsPatt:                      ; check if all LEDs are on    
     LDS     R16, CurrGameLEDsPatt           ; get the current game LEDs pattern
-    CPI     R16, GAME_OVER_LED_PATT         ; and compare it with game over patt
-    BREQ    ProcessWinGame                  ; if all LEDs are off, game is won 
+    LDS     R17, CurrGameLEDsPatt+1
+    CPI     R16, LOW(GAME_OVER_LED_PATT)    ; and compare it with game over patt
+    BRNE    CheckMoveCounter                ; if low byte no match, skip
+    CPI     R17, HIGH(GAME_OVER_LED_PATT)
+    BRNE    CheckMoveCounter                ; if high byte no match, skip
+    RJMP    GetWinMessage                   ; else pattern matches, game is won 
                                             ; NOTE: this should occur even if
                                             ; the move counter hit the max
-CheckMoveCounter:                       ; 
-    LDS     R16, MoveCounter
-    CPI     R16, GAME_OVER_MOVE_COUNT
-    BREQ    ProcessLoseGame             ; if move counter is at 0, player lost
+CheckMoveCounter:                       ; check if no moves left
+    LDS     R16, MoveCounter                ; get the current move count
+    LDS     R17, MoveCounter+1
+    CPI     R16, LOW(GAME_OVER_MOVE_COUNT)  ; compare it with game over count
+    BRNE    DoneGameOver                    ; if low byte no match, skip
+    CPI     R17, HIGH(GAME_OVER_MOVE_COUNT) 
+    BRNE    DoneGameOver                    ; if high byte no match, skip
+    ;RJMP   GetLoseMessage                  ; else move counter is 0, game lost
 
-ProcessWinGame:                         ; show WIN message and update game state
+GetLoseMessage:                         ;  get lose game over message
+    LDI     R16, LOW(GAME_LOSE_MESSAGE)
+    LDI     R17, HIGH(GAME_LOSE_MESSAGE)
+    RJMP    UpdateMessageAndState
 
-ProcessLoseGame:                        ; show LOSE message and update game state
+GetWinMessage:                          ; get win game over message
+    LDI     R16, LOW(GAME_WIN_MESSAGE)
+    LDI     R17, HIGH(GAME_WIN_MESSAGE)
+
+UpdateMessageAndState:                  ; set game over message and game state
+    STS     GameOverMessage, R16            ; store the game over message
+    STS     GameOverMessage+1, R17  
+    LDI     R16, GAME_OVER_STATE            ; load and store the game over state
+    STS     GameState, R16
 
 DoneGameOver:                       ; done checking if game is over
     RET                                 ; so return
@@ -262,21 +359,58 @@ DoneDelay16:                            ;done with the delay loop - return
 
 
 InitHexerGame:
-	RCALL	ResetMoveCounter
+
+InitHexerGameLEDs:                  ; initialize the starting game LEDs pattern
+    RCALL   ResetGameLEDs
+
+InitHexerMoveCounter:               ; initialize the starting move count
+	RCALL	ResetMoveCounter        
+
+InitHexerGameState:                 ; switch state to allow user to reset game
+    LDI     R16, GAME_OVER_STATE        ; load and store the game over state
+    STS     GameState, R16    
+    LDI     R16, LOW(GAME_LOSE_MESSAGE) ; load the lose message to display
+	LDI		R17, HIGH(GAME_LOSE_MESSAGE);
+    STS     GameOverMessage, R16        ; and store it
+	STS 	GameOverMessage+1, R17
+
+DoneInitHexerGame:                  ; done initializing HexerGame elements
+    RET                                 ; so return
 
 
-;
-ResetMoveCounter:
 
-InitMoveCounter:
-    LDI     R16, LOW(GAME_MAX_MOVE_COUNT)
-    LDI     R17, HIGH(GAME_MAX_MOVE_COUNT)
-    STS     MoveCounter, R16
-    STS     MoveCounter+1, R17
 
-DoneResetMoveCounter:
-    RET
+InvertGameLEDs:
 
+MaskCurrLEDsPatt:            ; apply the LED mask to the current game LEDs
+    LDS     R20, CurrGameLEDsPatt   ; load current game LEDs pattern
+    LDS     R21, CurrGameLEDsPatt+1 
+    AND     R20, R18                ; apply the mask in R19 | R18 to get shape
+    AND     R21, R19
+    COM     R20                     ; invert the selected bits
+    COM     R21
+	
+	AND		R20, R18				; keep only the selected bits
+	AND		R21, R19	
+   
+    COM     R18                         ; invert the mask bytes
+    COM     R19
+   
+    LDS     R16, CurrGameLEDsPatt       ; load current game LEDs pattern
+    LDS     R17, CurrGameLEDsPatt+1     
+   
+    AND     R16, R18                    ; clear all the bits set in the mask
+    AND     R17, R19
+
+    OR      R16, R20                    ; and apply the new shape pattern to
+    OR      R17, R21                    ; the game LED pattern (R17 - R16)
+	
+	STS		CurrGameLedsPatt, R16
+	STS		CurrGameLedsPatt+1, R17
+
+DoneInvertGameLEDs:                    ; done rotating the shape 
+    RCALL UpdateDisplay                 ; update the game LEDs and move counter 
+    RET                                 ; so return 
 
 
 RotateShape:
@@ -313,33 +447,38 @@ UseNewGameLEDsPatt:                 ; Found next pattern, store and display it
     AND     R16, R18                    ; clear all the bits set in the mask
     AND     R17, R19
 
-    OR     R16, R20                    ; and apply the new shape pattern to
-    OR     R17, R21                    ; the game LED pattern (R17 - R16)
+    OR      R16, R20                    ; and apply the new shape pattern to
+    OR      R17, R21                    ; the game LED pattern (R17 - R16)
 
     STS     CurrGameLEDsPatt, R16       ; and store the pattern as well to data
     STS     CurrGameLEDsPatt+1, R17
-    RJMP    DisplayNextGameLEDsPatt     ; now display it
+    RJMP    DoneRotateShape             ; now display it
 
 GoToNextRotationTableRow:           ; No match, so move to next row
     ADIW    ZL, ROTATION_TABLE_ROW_OFFSET   ; advance index to next row in table
     RJMP    FindNextGameLEDsPattLoop        ; and loop
 
-DisplayNextGameLEDsPatt:            ; have the next pattern, now display it
-    RCALL DisplayGameLEDs  
-
-UpdateMoveCounter:                  ; since next pattern is found, button press  
-    LDS     R16, MoveCounter            ; was valid, so decrement move counter
-    LDS     R16, MoveCounter+1
-
-    DEC     R16
-    STS     MoveCounter, R16
 DoneRotateShape:                    ; done rotating the shape 
+    RCALL UpdateDisplay                 ; update the game LEDs and move counter 
     RET                                 ; so return 
 
 
+UpdateDisplay:
 
-
-
+UpdateGameLEDs:
+    RCALL DisplayGameLEDs  
+UpdateMoveCounter:
+    LDS     YL, MoveCounter
+    LDS     YH, MoveCounter+1
+    SBIW    Y, 1          
+    STS     MoveCounter, YL
+    STS     MoveCounter+1, YH
+	MOV 	R16, YL
+	MOV		R17, YH
+	RCALL 	DisplayHex
+	
+	RET 
+	
 
 SwitchHandlerTable:
 ;    Switch Pattern     LED Mask            Table to Load             Function      
@@ -348,10 +487,11 @@ SwitchHandlerTable:
 .DW  RED_SW_PATT,    RED_LEDS_MASK,    (2 * RedTriangleRotation),   RotateShape
 .DW  GREEN_SW_PATT,  GREEN_LEDS_MASK,  (2 * GreenTriangleRotation), RotateShape
 .DW  BLUE_SW_PATT,   BLUE_LEDS_MASK,   (2 * BlueHexagonRotation),   RotateShape
-
+.DW  WHITE_SW_PATT,  WHITE_LEDS_MASK,   0,                        InvertGameLEDs
 ; data segment
 .dseg
 ;
-MoveCounter:         .BYTE   2 
-CurrGameLEDsPatt:    .BYTE   2
-
+MoveCounter:        .BYTE   2 
+CurrGameLEDsPatt:   .BYTE   2
+GameOverMessage:    .BYTE   2
+GameState:          .BYTE   1
